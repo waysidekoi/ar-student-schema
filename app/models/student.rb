c25c537b96fe2cead15ec15a8db95304875dc6da
6e0r9e <george@6e0r9e.com>
2013-03-29 16:06:46 -0700

    establishing many to many relationship

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,7 +6,7 @@ class Student < ActiveRecord::Base
   validates_numericality_of :age, :greater_than_or_equal_to => 5
   validates :phone, :format => { :with => /^[\\(]{0,1}([0-9]){3}[\\)]{0,1}[ ]?([^0-1]){1}([0-9]){2}[ ]?[-]?[ ]?([0-9]){4}[ ]*((x){0,1}([0-9]){1,5}){0,1}$/ }
 
-  belongs_to :teacher
+  has_and_belongs_to_many :teachers, join_table: :student_teachers
 
   def name
     "#{first_name} #{last_name}"
@@ -17,3 +17,4 @@ class Student < ActiveRecord::Base
     now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
   end
 end
+
